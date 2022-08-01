65609cf6e4588d760e7b5199627bb33860390b7f
Bruno Marques <brunorcmarques@gmail.com>
2022-08-01 19:08:10 -0300

    Requisito 2 - Adicona elemento Loading e salva usuario no Local Storage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,69 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 export default class Login extends Component {
+  state = {
+    nameInput: '',
+    loading: false,
+  };
+
+  updateName = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      nameInput: value,
+    });
+  }
+
+  handleClick = async () => {
+    const { history } = this.props;
+    const { nameInput } = this.state;
+    await this.setState({
+      loading: true,
+    }, async () => {
+      await createUser({ name: nameInput });
+    });
+    this.setState({ loading: false }, () => history.push('/search'));
+  }
+
   render() {
+    const { nameInput, loading } = this.state;
+    const minCaracAllowed = 3;
+
     return (
       <div data-testid="page-login">
-        Login
+
+        { loading
+          ? <Loading />
+          : (
+            <form>
+              <label htmlFor="login-name-input">
+                Name
+                <input
+                  type="text"
+                  data-testid="login-name-input"
+                  onChange={ this.updateName }
+                />
+              </label>
+
+              <button
+                type="button"
+                data-testid="login-submit-button"
+                disabled={ nameInput.length < minCaracAllowed }
+                onClick={ this.handleClick }
+              >
+                Entrar
+              </button>
+            </form>)}
+
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
