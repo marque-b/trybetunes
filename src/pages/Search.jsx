ba6f69830eca2ccfad048593776af4ce661275a8
Bruno Marques <brunorcmarques@gmail.com>
2022-08-01 22:57:05 -0300

    Requisito 5 - Adiciona input search artist

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,39 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    searchArtistInput: '',
+  }
+
+  updateArtistName = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      searchArtistInput: value,
+    });
+  }
+
   render() {
+    const { searchArtistInput } = this.state;
+    const minCharacAllowed = 2;
+
     return (
       <div data-testid="page-search">
         <Header />
         <p>Search</p>
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.updateArtistName }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ searchArtistInput.length < minCharacAllowed }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
